b255c318b484b2b0d8011550604e6adf185ef5e0
Knotto <one2treego@hotmail.com>
2016-10-01 06:43:06 +0000

    add structure database

--- js/databaseStructure.js
+++ js/databaseStructure.js
@@ -5,21 +5,34 @@ var $pageEntity = {
 	 getColumns : function(tablename){
 	 			var returnobj;
 	 			switch (tablename){
-	 				case "acct_no":
+	 				case "acct_fund": //1
+	 								returnobj ={
+	 											primary :"tn_id",
+	 											value : 0,
+	 											tn_id :"tn_id",
+	 											acctype_id:"acctype_id", //from acc_type
+	 											acct_id:"acct_id",  //from acct_no
+	 											tn_date:"tn_date",
+	 											tn_detail:"tn_detail",
+	 											amount:"amount",
+	 											remark:"remark",
+	 								};
+	 								break;
+	 				case "acct_no"://2
 	 								returnobj ={
 	 											primary :"acct_id",
 	 											value : 0,
 	 											acct_id :"acct_id",
 	 											acct_name:"acct_name",
 	 											acct_level:"acct_level",
-	 											acct_group:"acct_group",
+	 											acct_group:"acct_group", 
 	 								};
 	 								break;
-	 				case "acct_start":
+	 				case "acct_start"://3
 	 								returnobj ={
 	 											primary :"acctype_id",
 	 											value : 0,
-	 											acctype_id:"acctype_id",
+	 											acctype_id:"acctype_id",//From acct_type
 	 											accyear:"accyear",
 	 											accmount:"accmount",
 	 											balance_backward:"balance_backward",
@@ -27,46 +40,292 @@ var $pageEntity = {
 	 											balance_forward : "balance_forward"
 	 								};
 	 								break;
-	 				case "acct_type" :
+	 				case "acct_type" : //4
 	 								returnobj ={
-
+	 											primary :"acctype_id",
+	 											value : 0,
+	 											acctype_id:"acctype_id",
+	 											acctype_name:"acctype_name",
+	 											fund_id:"fund_id",//From funds
 	 								};
 	 								break;
-	 				case "agriculture_goods_dtl":
+	 				case "agriculture_goods_dtl"://5
 	 								returnobj ={
-
+	 											primary :"dtl_id",
+	 											value : 0,
+	 											dtl_id:"dtl_id",
+	 											buy_id:"buy_id",
+	 											buy_detail:"buy_detail",
+	 											amount:"amount",
+	 											buy_unit : "buy_unit",
+	 											price_unit : "price_unit"
 	 								};
 	 								break;
-	 				case "agriculture_goods_tn":
+	 				case "agriculture_goods_tn"://6
 	 								returnobj ={
-
+	 											primary :"buy_id",
+	 											value : 0,
+	 											buy_id:"buy_id",
+	 											buy_date:"buy_date",
+	 											buy_vender:"buy_vender",
+	 											acct_flag:"acct_flag",
 	 								};
 	 								break;	
-	 				case "asset_if":
+	 				case "asset_if"://7
 	 								returnobj ={
-
+	 											primary :"asset_id",
+	 											value : 0,
+	 											asset_id:"asset_id",
+	 											tn_date:"tn_date",
+	 											asset_detail:"asset_detail",
+	 											asset_amt:"asset_amt",
+	 											asset_unit : "asset_unit",
+	 											asset_unit_cost : "asset_unit_cost",
+	 											acctype_id:"acctype_id",
+	 											acct_flag : "acct_flag",
 	 								};
 	 								break;
-	 				case "funds":
+	 				case "fertilizer"://8
 	 								returnobj ={
-
+	 											primary :"fertilizer_id",
+	 											value : 0,
+	 											fertilizer_id:"fertilizer_id",
+	 											fertilizer_formula:"fertilizer_formula",
+	 											brand:"brand",
+	 											amount:"amount",
+	 											cost : "cost",
+	 											price : "price",
+	 											year_import : "year_import"
+	 								};
+	 								break;	 								
+	 				case "fertilizer_sales_dtl"://9
+	 								returnobj ={
+	 											primary :"sales_id",
+	 											value : 0,
+	 											sales_id:"sales_id",//from fertilizer_sales_tn
+	 											fertilizer_id:"fertilizer_id",
+	 											sales_total:"sales_total",
+	 											cost_unit:"cost_unit",
+	 											remark : "remark",
 	 								};
 	 								break;
-	 				case "goods":
+	 				case "fertilizer_sales_tn"://10
 	 								returnobj ={
-
+	 											primary :"sales_id",
+	 											value : 0,
+	 											sales_id:"sales_id",
+	 											sales_detail:"sales_detail",
+	 											address_id:"address_id",
+	 											address_moo:"address_moo",
+	 											sales_date : "sales_date",
+	 											sales_total : "sales_total",
+	 											net_total : "net_total",
+	 											remark : "remark",
+	 								};
+	 								break;	 								
+	 				case "funds"://11
+	 								returnobj ={
+	 											primary :"fund_id",
+	 											value : 0,
+	 											fund_id:"fund_id",
+	 											fund_name:"fund_name",
+	 											history:"history",
 	 								};
 	 								break;
-	 				case "goods_tf":
+	 				case "goods"://12
 	 								returnobj ={
-
+	 											primary :"goods_id",
+	 											value : 0,
+	 											goods_id:"goods_id",
+	 											goods_name:"goods_name",
+	 											accmount:"accmount",
 	 								};
 	 								break;
-	 				case "participant_tf":
+	 				case "goods_tf"://13
 	 								returnobj ={
-
+	 											primary :"tf_id",
+	 											value : 0,
+	 											tf_id:"tf_id",
+	 											tn_date:"tn_date",
+	 											goods_id:"goods_id",
+	 											goods_amt:"goods_amt",
+	 											acctype_id : "acctype_id",
+	 											goods_price : "goods_price"
 	 								};
-	 								break;																
+	 								break;
+	 				case "milling_fund_summary"://14
+	 								returnobj ={
+	 											primary :"summary_key",
+	 											value : 0,
+	 											summary_key:"summary_key",
+	 											summary_date:"summary_date",
+	 											net_amount:"net_amount",
+	 											net_weight_paddy:"net_weight_paddy",
+	 											net_weight_rice : "net_weight_rice",
+	 											net_broken_milled_rice : "net_broken_milled_rice",
+	 											net_rice_bran : "net_rice_bran",
+	 								};
+	 								break;
+	 				case "milling_fund_tn"://15
+	 								returnobj ={
+	 											primary :"tn_id",
+	 											value : 0,
+	 											tn_id:"tn_id",
+	 											milling_date:"milling_date",
+	 											milling_detail:"milling_detail",
+	 											milling_address:"milling_address",
+	 											amount : "amount",
+	 											weight_paddy : "weight_paddy",
+	 											weight_rice : "weight_rice",
+	 								};
+	 								break;
+	 				case "mill_expense_tn"://16
+	 								returnobj ={
+	 											primary :"tn_id",
+	 											value : 0,
+	 											tn_id:"tn_id",
+	 											tn_date:"tn_date",
+	 											tn_detail:"tn_detail",
+	 											mill_expense_id:"mill_expense_id",
+	 											units : "units",
+	 											amount : "amount",
+	 											price_per_unit:"price_per_unit",
+	 											remark : "remark",
+	 								};
+	 								break;	 								
+	 				case "mill_expense_transf"://17
+	 								returnobj ={
+	 											primary :"tn_id",
+	 											value : 0,
+	 											tn_id:"tn_id",
+	 											tn_date:"tn_date",
+	 											tn_detail:"tn_detail",
+	 											net:"net",
+	 											remark : "remark",
+	 								};
+	 								break;
+	 				case "mill_expense_transf_dtl"://18
+	 								returnobj ={
+	 											primary :"tn_id",
+	 											value : 0,
+	 											tn_id:"tn_id",
+	 											rice_id:"rice_id",
+	 											amount:"amount",
+	 											price_per_unit:"price_per_unit",
+	 											transf_amount : "transf_amount",
+	 								};
+	 								break;
+	 				case "mill_expense_type"://19
+	 								returnobj ={
+	 											primary :"mill_expense_id",
+	 											value : 0,
+	 											mill_expense_id:"mill_expense_id",
+	 											expense_name:"expense_name",
+	 								};
+	 								break;
+	 				case "mill_fund_board"://20
+	 								returnobj ={
+	 											primary :"board_id",
+	 											value : 0,
+	 											board_id:"board_id",
+	 											board_name:"board_name",
+	 											board_position:"board_position",
+	 											board_status:"board_status",
+	 								};
+	 								break;
+	 				case "mill_fund_member"://21
+	 								returnobj ={
+	 											primary :"member_id",
+	 											value : 0,
+	 											member_id:"member_id",
+	 											prename:"prename",
+	 											first_name:"first_name",
+	 											last_name:"last_name",
+	 											address_no : "address_no",
+	 											moo : "moo",
+	 											sub_district:"sub_district",
+	 											district:"district",
+	 											province:"province",
+	 											share_amount:"share_amount",
+	 											share_price_unit : "share_price_unit",
+	 											apply_date:"apply_date",
+	 											active_flag:"active_flag",
+	 											remark : "remark",
+	 								};
+	 								break;	 								
+	 				case "mill_income_dtl"://22
+	 								returnobj ={
+	 											primary :"dtl_id",
+	 											value : 0,
+	 											dtl_id:"dtl_id",
+	 											tn_id:"tn_id",
+	 											mill_product_id:"mill_product_id",
+	 											amount:"amount",
+	 											price_per_unit : "price_per_unit",
+	 								};
+	 								break;
+	 				case "mill_income_tn"://23
+	 								returnobj ={
+	 											primary :"tn_id",
+	 											value : 0,
+	 											tn_id:"tn_id",
+	 											tn_detail:"tn_detail",
+	 											tn_date:"tn_date",
+	 											remark:"remark",
+	 											net_sale : "net_sale",
+	 								};
+	 								break;
+	 				case "mill_product"://24
+	 								returnobj ={
+	 											primary :"mill_product_id",
+	 											value : 0,
+	 											mill_product_id:"mill_product_id",
+	 											product_name:"product_name",
+	 											cost:"cost",
+	 											price:"price",
+	 											amount : "amount",
+	 								};
+	 								break;
+	 				case "observe_activity_tn"://25
+	 								returnobj ={
+	 											primary :"obs_activity_id",
+	 											value : 0,
+	 											obs_activity_id:"obs_activity_id",
+	 											obs_date:"obs_date",
+	 											obs_detail:"obs_detail",
+	 											obs_amount:"obs_amount",
+	 											obs_expense : "obs_expense",
+	 											obs_remark : "obs_remark"
+	 								};
+	 								break;
+	 				case "operating_schedule"://26
+	 								returnobj ={
+	 											primary :"tn_id",
+	 											value : 0,
+	 											tn_id:"tn_id",
+	 											operate_date:"operate_date",
+	 											board_id:"board_id",
+	 											remark:"remark",
+	 								};
+	 								break;	 								
+	 				case "participant_tf"://27
+	 								returnobj ={
+	 											primary :"pt_id",
+	 											value : 0,
+	 											pt_id:"pt_id",
+	 											tn_date:"tn_date",
+	 											join_name:"join_name",
+	 											acctype_id:"acctype_id",
+	 								};
+	 								break;	
+					case "rice"://28
+	 								returnobj ={
+	 											primary :"rice_id",
+	 											value : 0,
+	 											rice_id:"rice_id",
+	 											rice_name:"rice_name",
+	 								};
+	 								break;		 								
 	 				default :
 				 				returnobj = undefined;
 	 			}
